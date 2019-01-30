b49b63d3226fee3128fe67f217c2f8f4ce038fd8
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-01-30 13:53:24 +0100

    Allow editing in the view.

--- src/com/ciplogic/et/magicgroup/views/GroupView.java
+++ src/com/ciplogic/et/magicgroup/views/GroupView.java
@@ -9,13 +9,21 @@ import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.part.DrillDownAdapter;
@@ -23,6 +31,7 @@ import org.eclipse.ui.part.ResourceTransfer;
 import org.eclipse.ui.part.ViewPart;
 
 import com.ciplogic.et.magicgroup.model.GroupComparator;
+import com.ciplogic.et.magicgroup.model.TreeItem;
 import com.ciplogic.et.magicgroup.views.actions.AddNewGroupAction;
 import com.ciplogic.et.magicgroup.views.actions.EditDescriptionAction;
 import com.ciplogic.et.magicgroup.views.actions.NodeDoubleClickAction;
@@ -32,6 +41,8 @@ import com.ciplogic.et.magicgroup.views.providers.ViewContentProvider;
 import com.ciplogic.et.magicgroup.views.providers.ViewLabelProvider;
 import com.ciplogic.et.magicgroup.views.sorters.NameComparator;
 
+import net.miginfocom.swt.MigLayout;
+
 public class GroupView extends ViewPart {
 	private TreeViewer treeViewer;
 	private DrillDownAdapter drillDownAdapter;
@@ -39,6 +50,9 @@ public class GroupView extends ViewPart {
 	private Action removeNewGroupAction;
 	private Action doubleClickAction;
 	private Action editDescriptionAction;
+
+	private StyledText descriptionEdit;
+	private Text shortDescriptionEdit;
 
 	/**
 	 * The constructor.
@@ -51,12 +65,23 @@ public class GroupView extends ViewPart {
 	 * to create the viewer and initialize it.
 	 */
 	public void createPartControl(Composite parent) {
+		parent.setLayout(new MigLayout("", "[grow]", "[grow][][]"));
+
 		treeViewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
 		drillDownAdapter = new DrillDownAdapter(treeViewer);
 		treeViewer.setContentProvider( new ViewContentProvider(this) );
 		treeViewer.setLabelProvider(new ViewLabelProvider());
 		treeViewer.setComparator(new NameComparator());
 		treeViewer.setInput(getViewSite());
+		treeViewer.getTree().setLayoutData("wrap, growx, growy");
+
+		descriptionEdit = new StyledText(parent, SWT.BORDER);
+		descriptionEdit.setText("Description");
+		descriptionEdit.setLayoutData("height 200!, wrap, growx, south");
+
+		shortDescriptionEdit = new Text(parent, SWT.BORDER);
+		shortDescriptionEdit.setText("Short Description");
+		shortDescriptionEdit.setLayoutData("growx, south, wrap");
 		
 		// XXX: for crying out loud, who designed this API?
 		
@@ -65,6 +90,7 @@ public class GroupView extends ViewPart {
 		makeActions();
 		hookContextMenu();
 		hookDoubleClickAction();
+		hookUpdateDescription();
 		contributeToActionBars();
 		addDropListener();
 	}
@@ -134,6 +160,50 @@ public class GroupView extends ViewPart {
 		treeViewer.addDoubleClickListener(new IDoubleClickListener() {
 			public void doubleClick(DoubleClickEvent event) {
 				doubleClickAction.run();
+			}
+		});
+	}
+
+	private void hookUpdateDescription() {
+		treeViewer.addSelectionChangedListener(new ISelectionChangedListener() {
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {				
+				ISelection selection = event.getSelection();
+				
+				if (selection == null || selection.isEmpty()) {
+					descriptionEdit.setText("Description");
+					descriptionEdit.setEnabled(false);
+					shortDescriptionEdit.setText("Short Descrpition");
+					shortDescriptionEdit.setEnabled(false);
+				} else {
+					TreeItem file = (TreeItem) ((IStructuredSelection)selection).getFirstElement();				
+
+					shortDescriptionEdit.setText(file.getShortDescription());
+					shortDescriptionEdit.setEnabled(true);
+					descriptionEdit.setText(file.getDescription());
+					descriptionEdit.setEnabled(true);
+				}				
+			}
+		});
+		
+		descriptionEdit.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				IStructuredSelection selection = (IStructuredSelection) treeViewer.getSelection();
+				TreeItem selectedTreeItem = (TreeItem) selection.getFirstElement();
+
+				selectedTreeItem.setDescription(descriptionEdit.getText());
+			}
+		});
+		
+		shortDescriptionEdit.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				IStructuredSelection selection = (IStructuredSelection) treeViewer.getSelection();
+				TreeItem selectedTreeItem = (TreeItem) selection.getFirstElement();
+
+				selectedTreeItem.setShortDescription(shortDescriptionEdit.getText());
+				treeViewer.refresh();
 			}
 		});
 	}
